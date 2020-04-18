570cd662f4ba38d3990708e1d27a7ebb872598f8
Depressitch <42008237+Depressitch@users.noreply.github.com>
2020-04-19 00:05:38 +0300

    added base code

---
+++ newTrello/Task.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrelloClone
+{
+
+    public class Task
+    {
+        public Statuses Status;
+        public string ResponsibleUser;
+        public string Text;
+        public string Title;
+
+        public Task()
+        {
+            Text = "";
+        }
+        public void ShowStatuses()
+        {
+            int i = 0; 
+            foreach (var status in Enum.GetValues(typeof(Statuses)))
+            {
+                Console.WriteLine($"{i++}: {status}");
+            }
+        }
+        public enum Statuses
+        {
+            ToDo,
+            OnTeacher,
+            OnStudent,
+            Closed,
+        }
+    }
+}
+
+
+//!карточка:
+//!отдельный класс, имеет параметры:
+//!статус, исполнитель, текст
